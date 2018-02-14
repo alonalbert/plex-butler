0fdbeb5f0d66de41050dc61fffe1d7fb351d74f3
Alon Albert <alon.albert@gmail.com>
2018-02-14 14:46:31 +0200

    Start working on fix match

---
+++ app/src/main/java/com/alonalbert/plexbutler/ui/FixMatchActivity.java
@@ -0,0 +1,12 @@
+package com.alonalbert.plexbutler.ui;
+
+import android.support.v7.app.AppCompatActivity;
+
+import com.alonalbert.plexbutler.R;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_fix_match)
+public class FixMatchActivity extends AppCompatActivity {
+
+}
